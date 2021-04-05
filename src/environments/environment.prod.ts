423388a7ab2d27cb09d1a6379f4885eb5eae8db9
Hakan YILDIZ <hakanyildiz@anadolu.edu.tr>
2021-04-05 21:31:23 +0300

    Dependencies installed. Custom Material Theme impl.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,29 @@
+import { KeycloakOptions } from 'keycloak-angular';
+
+const keycloakOptions: KeycloakOptions = {
+  config: {
+    realm: '####-REALM',
+    url: window['restApiURL'] + '/auth/',
+    clientId: window['keycloakClientID']
+  },
+  initOptions: {
+    onLoad: 'login-required',
+    checkLoginIframe: false,
+    enableLogging: true
+  },
+  enableBearerInterceptor: true,
+  bearerExcludedUrls: ['/assets', '/clients/public'],
+  loadUserProfileAtStartUp: false
+};
+
 export const environment = {
-  production: true
+  production: true,
+  APP_NAME: window['appName'],
+  API_BASE_PATH: window['restApiURL'] + '/api/',
+  keycloakOptions,
+  PATH: {
+    PATH1: 'path1',
+    PATH2: 'path2',
+    PATH3: 'path3'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,34 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { KeycloakOptions } from 'keycloak-angular';
+
+const keycloakOptions: KeycloakOptions = {
+  config: {
+    realm: '####-REALM',
+    url: window['restApiURL'] + '/auth/',
+    clientId: window['keycloakClientID']
+  },
+  initOptions: {
+    onLoad: 'login-required',
+    checkLoginIframe: false,
+    enableLogging: true
+  },
+  enableBearerInterceptor: true,
+  bearerExcludedUrls: ['/assets', '/clients/public'],
+  loadUserProfileAtStartUp: false
+};
+
 export const environment = {
-  production: false
+  production: false,
+  APP_NAME: window['appName'],
+  API_BASE_PATH: window['restApiURL'] + '/api/',
+  keycloakOptions,
+  PATH: {
+    PATH1: 'path1',
+    PATH2: 'path2',
+    PATH3: 'path3'
+  }
 };
 
 /*
